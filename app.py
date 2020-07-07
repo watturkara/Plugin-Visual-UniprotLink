41e4e63c4c2f2b87bdeea4d3eb2ece55bf0a5b53
JMante1 <40006518+JMante1@users.noreply.github.com>
2020-07-07 12:42:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def evaluate():
 @app.route("/run", methods=["POST"])
 def run():
     data = request.get_json(force=True)
-    return jsonify(data)
+    return( jsonify(data))
     #cwd = os.getcwd()
     #filename = os.path.join(cwd, "Test.html")
     #
