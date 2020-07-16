e6b84cccaa573223de3214b97cc15f02303f1009
JMante1 <40006518+JMante1@users.noreply.github.com>
2020-07-16 16:25:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,10 +15,15 @@ def evaluate():
 def run():
     data = request.get_json(force=True)
     
-    url = data['complete_sbol'].replace('/sbol','')
-    instance = data['instanceUrl']
-    uri = data['top_level']
+    top_level_url = data['top_level']
+    complete_sbol = data['complete_sbol']
+    instance_url = data['instanceUrl']
+    size = data['size']
+    rdf_type = data['type']
+    shallow_sbol = data['shallow_sbol']
     
+    url = complete_sbol.replace('/sbol','')
+      
     cwd = os.getcwd()
     filename = os.path.join(cwd, "Test.html")
     
@@ -28,8 +33,8 @@ def run():
             
         #put in the url, uri, and instance given by synbiohub
         result = result.replace("URL_REPLACE", url)
-        result = result.replace("URI_REPLACE", uri)
-        result = result.replace("INSTANCE_REPLACE", instance)
+        result = result.replace("URI_REPLACE", top_level_url)
+        result = result.replace("INSTANCE_REPLACE", instance_url)
            
         result = result.replace("REQUEST_REPLACE", str(data))
             
